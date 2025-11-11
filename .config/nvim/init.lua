a96d77aa7a44a47c5d44779e04592aa22a37bcb1
Travis Luckenbaugh <tluckenbaugh@gmail.com>
2025-11-11 11:27:36 -0500

    LSP/Elixir configs

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -1,6 +1,7 @@
 vim.g.mapleader = ' '
 vim.g.maplocalleader = ' '
 
+vim.opt.clipboard = 'unnamedplus'
 vim.opt.number = true       
 vim.opt.relativenumber = true
 
@@ -13,6 +14,11 @@ Plug 'nvim-treesitter/nvim-treesitter'
 Plug 'junegunn/fzf'
 Plug 'junegunn/fzf.vim'
 
+Plug 'elixir-editors/vim-elixir'	" Syntax highlighting
+Plug 'neovim/nvim-lspconfig'		" LSP support
+Plug 'hrsh7th/nvim-cmp'			" Autocompletion
+Plug 'hrsh7th/cmp-nvim-lsp'	" LSP completion source
+
 call plug#end()
 ]]
 
@@ -24,3 +30,21 @@ vim.keymap.set('n', '<leader>gg', ':G<CR>') -- Interactive git
 vim.keymap.set('n', '<leader>gc', ':Git commit<CR>')
 vim.keymap.set('n', '<leader>gp', ':Git push<CR>')
 vim.keymap.set('n', '<leader>gb', ':Git blame<CR>')
+
+vim.lsp.config.elixirls = {
+  cmd = { 'elixir-ls' },
+  filetypes = { 'elixir', 'eelixir', 'heex', 'surface' },
+  root_dir = vim.fs.root(0, { 'mix.exs', '.git' }),
+}
+
+vim.lsp.enable('elixirls')
+
+vim.api.nvim_create_autocmd('LspAttach', {
+  callback = function(args)
+    local opts = { buffer = args.buf }
+    vim.keymap.set('n', 'gd', vim.lsp.buf.definition, opts)
+    vim.keymap.set('n', 'K', vim.lsp.buf.hover, opts)
+    vim.keymap.set('n', '<leader>rn', vim.lsp.buf.rename, opts)
+    vim.keymap.set('n', 'gr', vim.lsp.buf.references, opts)
+  end,
+})
