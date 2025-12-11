d443ca7c14d1231b82f0d44068e88c1e761052a5
Travis Luckenbaugh <tluckenbaugh@gmail.com>
2025-12-10 19:12:24 -0500

    Install revamp

--- bin/macakase.sh
+++ bin/macakase.sh
@@ -1,16 +1,6 @@
 #!/bin/sh
 # Install various dev tools and configs for a Mac
 
-# WARNING: This is a work in progress!
-# It's based on DHH's Omakase, BUT:
-# - I don't agree / haven't tried everything in there - items I haven't cleared are TODOs or [wrapped]
-# - I'm not (yet) leaving Mac
-# - These are only additive changes 
-#   - no tiling WMs, 
-#   - no replacing Mac keybinds with Omarchy keybinds
-#   - explicitly disabled items marked with NOOP
-# zsh ~/Code/travis-mark/dotfiles/bin/macakase.sh
-
 # Grab directory of running script
 if [ -n "${BASH_SOURCE[0]}" ]; then
     SCRIPT_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
@@ -27,65 +17,26 @@ if ! command -v brew &> /dev/null; then
         echo 'eval "$(/opt/homebrew/bin/brew shellenv)"' >> ~/.zprofile
         eval "$(/opt/homebrew/bin/brew shellenv)"
     fi
+    echo "Homebrew installed"
 else
-    echo "Homebrew already installed"
+    echo "Homebrew installation found"
 fi
 
-# Create config directory
-mkdir -p ${HOME}/.macakase
-
-# NOOP: Tiling WM
-
-# NOOP: Hotkeys - https://learn.omacom.io/2/the-omarchy-manual/53/hotkeys
-
-# TODO: Desktop background cycler
-# TODO: Color themed apps
-# TODO: File manager
-
-# TODO: Neovim (defaults?)
-
-# TODO: Quick Emojis / Emojis
-
-# TODO: Completions 
-
-# Setup shell tools
-echo "Setup Shell Tools (fzf zoxide rg eza fd gron)"
+# Install packages from Homebrew
+echo "Install packages from Homebrew"
 brew install fzf zoxide rg eza fd gron
+brew install lazygit
+brew install nvim alacritty gh 
+brew services start rconroy293/seventeenlands/seventeenlands
+
+# Config directory setup (~/.macakase)
+echo "Config directory setup (~/.macakase)"
+mkdir -p ${HOME}/.macakase
 fzf --zsh > ~/.macakase/fzf.zsh
-zoxide init --cmd cd zsh > ~/.macakase/zoxide.zsh
+zoxide init --cmd z zsh > ~/.macakase/zoxide.zsh
 cp ${SCRIPT_DIR}/macakase-eza.zsh ~/.macakase/eza.zsh
 cp ${SCRIPT_DIR}/macakase-init.zsh ~/.macakase/__init__.zsh
 source ~/.zshrc # TODO: Add init installer
-echo "Completed Shell Tools - \`open ~/.macakase to view configs\`"
 
-# TODO: Shell Functions - compress iso2sd format-disk img2jpg img2jpg-small img2png
-
-# TODO: TUIs - Lazygit [lazydocker btop impala fastfetch]
-brew install lazygit
-# TODO: GUIs (O) - Obsidian Pinta LocalSend LibreOffice Signal mpv "OBS Studio" Kdenlive 
-# TODO: GUIs (C) - 1Password Typora Dropbox Tailscale Spotify
-# TODO: Dev Tools - Neovim Alacritty [Mise Docker] github-cli
-brew install nvim alacritty gh 
-# TODO: PDFs - Document Viewer Xournal++
-
-# TODO: Web App wrapping - WhatsApp Claude YouTube
-
-# TODO: Games - Steam RetroArch Minecraft (!) Doom
-
-# TODO: Windows VM
-
-# TODO: Brew GUI
-
-# TODO: Dotfiles / config changing / Defaults explorer
-
-# NOOP: Fonts
-
-# NOOP: Multiple monitors
-
-# NOOP: Keyboard, mouse, trackpad
-
-# NOOP: Bioauth
-
-# TODO: Starship
-
-# TODO: Updates
+# Complete
+echo "Setup complete - \`open ~/.macakase\` to view configs"
